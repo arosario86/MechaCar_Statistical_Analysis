a66c0f28d542495941390c47ad4bf4c7d9fd5e34
Angelica Rosario <angelicarosario86@gmail.com>
2022-10-03 21:34:14 -0700

    New file.

--- Challenge/MechaCarChallenge.R
+++ Challenge/MechaCarChallenge.R
@@ -9,3 +9,15 @@ head(MechaCar_table)
 linear_model <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD + mpg, data=MechaCar_table)
 #summarize linear model
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD ,data=MechaCar_table))
+
+#Deliverable 2
+
+#import csv file
+Suspension_Coil_table <- read.csv(file='Suspension_Coil.csv', check.names=F, stringsAsFactors = F)
+head(Suspension_Coil_table)
+#import library
+library(tidyverse)
+#create summary table
+summarize_suspension <- Suspension_Coil_table %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI))
+#create lot summary table
+lot_summary <- Suspension_Coil_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI), .groups = 'keep')
